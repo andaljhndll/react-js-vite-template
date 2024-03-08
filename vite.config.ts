5e739febaae7ef6aca1bdc867682dca477f3a5dc
Jhondell B. Andal <andaljhndll@gmail.com>
2024-03-08 17:28:01 +0000

    refactor: Replaced file extension of vite.config from cjs to ts file

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+});
