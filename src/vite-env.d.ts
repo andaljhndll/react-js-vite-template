23d2d664601f45991904ef1e0c3a95a911ff6957
Jhondell B. Andal <andaljhndll@gmail.com>
2024-03-08 17:41:32 +0000

    feat: Defined type definitions for user-defined env variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_API_ORIGIN_URL: string;
+  readonly VITE_APP_API_AUTH_URL: string;
+  readonly VITE_APP_API_ADMIN_URL: string;
+  readonly VITE_APP_FETCH_TIMEOUT: number;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
