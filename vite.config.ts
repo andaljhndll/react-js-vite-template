c01be2f2a26b16aae03f870f01fb9361c1ceade4
Jhondell B. Andal <andaljhndll@gmail.com>
2024-04-29 14:44:35 +0000

    refactor: Removed tsconfigPaths from vite.config.ts and replaced it with path from NodeJS and added file-path aliases as well

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,19 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "node:path";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
+  },
+  resolve: {
+    alias: {
+      "@customTypes/*": path.resolve(__dirname, "./src/types"),
+      "@configs/*": path.resolve(__dirname, "./src/configs"),
+    },
   },
 });
